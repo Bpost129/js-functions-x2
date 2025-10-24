f6d0da2c2a9bf0126c43085346ed2165fefa0c96
Bpost129 <Bpost129@yahoo.com>
2025-10-23 20:16:53 -0400

    change bg color, write different function definitions

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-function getPointsScored(elapsedTime) {
-  if (elapsedTime < 30) {
-    return 100
-  } else if (elapsedTime < 60) {
-    return 75
-  } else {
-    return 25
-  }
+// function getPointsScored(elapsedTime) {
+//   if (elapsedTime < 30) {
+//     return 100
+//   } else if (elapsedTime < 60) {
+//     return 75
+//   } else {
+//     return 25
+//   }
+// }
+
+// const points = getPointsScored(61)
+
+// console.log(points)
+
+
+
+//function declaration
+//hoisted - can be called before it is defined
+// function sayHello(name) {
+//   console.log(`Hello ${name}!`)
+// }
+
+//function expression
+//cannnot be called before it is defined
+// let sayHello = function(name) {
+//   console.log(`Hello ${name}!`)
+// }
+
+//arrow function
+let sayHello = (name) => {
+  console.log(`Hello ${name}!`)
 }
 
-const points = getPointsScored(61)
-
-console.log(points)
+sayHello('Alice')
